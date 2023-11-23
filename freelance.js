5c4404420c6548f8c9d4dfdfadeadec2d99e69b8
Syaz S <syazski@gmail.com>
2023-11-23 11:53:20 -0600

    updated freelancer list

--- freelance.js
+++ freelance.js
@@ -10,11 +10,12 @@ const names = ["Carol", "Joe", "Matt", "Brittany", "Amber", "Lee", "Kate", "Wayn
 const occupations = ["gardener", "programmer", "teacher", "driver"];
 const prices =["90", "20", "50", "70","80", "30"]
 
-const maxFreelancers = 5;
+const maxFreelancers = 10;
+
+const freelancers2 =[];
 
 // create an interval to show freelancers2 every 1s
 const addFreelancerIntervalId = setInterval(addFreelancer, 2000);
-render();
 
 //select list1 from DOM, and show the first set of freelancers name, price and occupation
 const nameList = document.getElementById("name");
@@ -41,51 +42,50 @@ const freelancerOcc = freelancers.map((freelancer) => {
     return occElement;
 })
 
-// create function to update the average price on the array
+// create function to calculate the average price on the array
 function averagePrice() {
-  const sum = freelancers.reduce((accum,item) => accum + item.price, 0)
-  const avg = sum / freelancers.length;
+  let sum = 0;
+  for (let i = 0; i < freelancers.length; i++){
+    sum += freelancers[i].price;
+  };
+  let avg = sum / freelancers.length;
   return avg;
 };
 
-//Create a function to add a random freelancers to the freelancers array and print average price
+const displayPrice = document.querySelector("h2");
+displayPrice.innerHTML = `The average starting price is $${averagePrice()}`;
+
+//Create a function to add a random freelancers to the freelancers array
 function addFreelancer() {
   const name = names[Math.floor(Math.random() * names.length)];
   const price = prices[Math.floor(Math.random()* prices.length)];
   const occupation = occupations[Math.floor(Math.random()* occupations.length)];
   freelancers.push({name, price, occupation});
 
-  render();
+  const newLancer = [freelancers[freelancers.length -1]];
+  
+  const name2List = document.getElementById("name2");
+  const freelancer2List = newLancer.map((freelancer) => {
+    const name2Element = document.createElement("li");
+    name2Element.innerHTML = freelancer.name;
+    name2List.appendChild(name2Element);
+    });
+
+    const price2List = document.getElementById("price2");
+    const freelancer2Price = newLancer.map((freelancer) => {
+      const price2Element = document.createElement("li");
+      price2Element.innerHTML = `$ ${freelancer.price}`;
+      price2List.appendChild(price2Element);
+    });
+
+    const occ2List = document.getElementById("occ2");
+    const freelancer2Occ = newLancer.map((freelancer) => {
+      const occ2Element = document.createElement("li");
+      occ2Element.innerHTML = freelancer.occupation;
+      occ2List.appendChild(occ2Element);
+    });
 
   if(freelancers.length >= maxFreelancers) {
     clearInterval(addFreelancerIntervalId);
   };
-  }
-
-//create a render function 
-
-function render() {
-  const name2List = document.getElementById("name2");
-  const freelancer2List = freelancers.map((freelancer) => {
-    const name2Element = document.createElement("li");
-    name2Element.innerHTML = freelancer.name;
-    name2List.append(name2Element);
-    })
-
-    const price2List = document.getElementById("price2");
-    const freelancer2Price = freelancers.map((freelancer) => {
-      const price2Element = document.createElement("li");
-      price2Element.innerHTML = `$ ${freelancer.price}`;
-      price2List.append(price2Element);
-    })
-
-    const occ2List = document.getElementById("occ2");
-    const freelancer2Occ = freelancers.map((freelancer) => {
-      const occ2Element = document.createElement("li");
-      occ2Element.innerHTML = freelancer.occupation;
-      occ2List.append(occ2Element);
-    });
-
-    const avgPrice = document.querySelector("h2").innerHTML = `The average starting price is $${averagePrice()}`; 
-
 };
